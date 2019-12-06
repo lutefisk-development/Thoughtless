a5a2674ef912328689ecb88879c36eec868d745d
Tillmann Weimer <tillmann1970@gmail.com>
2019-12-06 10:48:04 +0100

    added code for displaying the widget on the page

--- public/wp-content/plugins/instagram/includes/class-instagram-widget.php
+++ public/wp-content/plugins/instagram/includes/class-instagram-widget.php
@@ -20,13 +20,27 @@ class Instagramwidget extends WP_Widget{
      */
     public function widget($args, $instance){
         extract($args);
-        $title = apply_filters('widget_title', $instance['title']);
+        //$title = apply_filters('widget_title', $instance['title']);
         echo $before_widget;
         if (! empty($title)) {
         echo $before_title . $title . $after_title;
         }
-        echo $instance['content'];
+        //echo $instance['content'];
         echo $after_widget;
+    
+    ?>
+    <div class="container">
+        <div class="col-4">
+            <h3>Instagram</h3>
+            <div class="row">
+                <p><?php _e('Popular pictures', 'instagram'); ?></p><br>
+                <div class="popimages">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
     }
 
     /**
@@ -39,12 +53,9 @@ class Instagramwidget extends WP_Widget{
         else {
             $title = __('New title', 'instagram');
         }
-        if (isset($instance['content'])) {
-            $content = $instance['content'];
-        }
-        else {
-            $content = '';
-        }
+    /**
+     * The field where you can insert your Instagram API token
+     */
         ?>
         <p>
         <label for="<?php echo $this->get_field_name('title'); ?>">
@@ -57,30 +68,29 @@ class Instagramwidget extends WP_Widget{
         type="text" 
         value="<?php echo esc_attr($title); ?>" />
         </p>
-        <p>
-        <label for="<?php echo $this->get_field_name('content'); ?>">
-            <?php _e('Content:'); ?>
-        </label>
-        <textarea 
-            class="widefat" 
-            id="<?php echo $this->get_field_id('content'); ?>" 
-            name="<?php echo $this->get_field_name('content'); ?>" 
-            rows="8"
-            ><?php echo $content; ?></textarea>
-        </p>
         <?php
+    /**
+     * /The field where you can insert your Instagram API token
+     */
 
 
     }
+    /**
+     * /Front End for displaying widget in wp-admin
+     */
 
+     /**
+      * The Method which saves your insertef Instagram API token
+      */
     public function update($new_instance, $old_instance){
         $instance = [];
         $instance['title'] = (!empty($new_instance['title'])) 
         ? strip_tags($new_instance['title']) 
         : '';
-        $instance['content'] = (!empty($new_instance['content'])) 
-        ? $new_instance['content'] 
-        : '';
+        
         return $instance;
     }
+    /**
+      * /The Method which saves your insertef Instagram API token
+      */
 }
