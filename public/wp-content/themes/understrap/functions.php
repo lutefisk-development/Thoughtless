682febf3fbcb2cd372787d6a8fffbe40abaaa794
pks-development <kristiansvanberg@gmail.com>
2019-12-07 13:35:13 +0100

    07-12-2019
    Bundled ACF plugin and fieldsgroups. also added image used for hero.

--- public/wp-content/themes/understrap/functions.php
+++ public/wp-content/themes/understrap/functions.php
@@ -18,6 +18,8 @@ $understrap_includes = array(
 	'/hooks.php',                           // Custom hooks.
 	'/extras.php',                          // Custom functions that act independently of the theme templates.
 	'/custom-post-types.php',               // Load Custom Post Types.
+	'/acf.php',                             // Load ACF Plugin.
+	'/acf-fieldgroups.php',                 // Load ACF Field Groups.
 	'/customizer.php',                      // Customizer additions.
 	'/custom-comments.php',                 // Custom Comments file.
 	'/jetpack.php',                         // Load Jetpack compatibility file.
