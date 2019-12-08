3bf96d84cdb4e1c22d8e495cc4e47383fe42c2d8
Joakim <jmalmberg.web@gmail.com>
2019-12-08 17:20:47 +0100

    added the ability to change access token in the widgets area w/Tillmann

--- public/wp-content/plugins/instagram/includes/class-instagram-widget.php
+++ public/wp-content/plugins/instagram/includes/class-instagram-widget.php
@@ -35,11 +35,12 @@ class Instagramwidget extends WP_Widget{
     public function widget($args, $instance){
         extract($args);
         $title = apply_filters('widget_title', $instance['title']);
+        $instagram_token = apply_filters('widget_title', $instance['instagram_token']);
         echo $before_widget;
         if (! empty($title)) {
             echo $before_title . $title . $after_title;
         }
-        $access_token = 'ACCESS_TOKEN';
+        $access_token = $instagram_token;
 
         $instagram_images = $this->curl_connect("https://graph.instagram.com/" . 'me' . '?fields=account_type,username,media&access_token=' . $access_token);
 
@@ -70,28 +71,44 @@ class Instagramwidget extends WP_Widget{
      * Front End for displaying widget in wp-admin
      */
     public function form($instance) {
-        if (isset($instance['title'])) {
-            $title = $instance['title'];
-        }
-        else {
-            $title = __('New title', 'instagram');
-        }
+      if (isset($instance['title'])) {
+          $title = $instance['title'];
+      }
+      else {
+          $title = __('New title', 'instagram');
+      }
     /**
      * The field where you can insert your Instagram API token
      */
+      if (isset($instance['instagram_token'])) {
+        $instagram_token = $instance['instagram_token'];
+      }
+      else {
+        $instagram_token = __('New instagram_token', 'instagram');
+      }
 
-        ?>
-  <p>
-    <label for="<?php echo $this->get_field_name('title'); ?>">
-      <?php _e('Title:'); ?>
-    </label>
-    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-      name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-  </p>
-  <?php
+
+      ?>
+        <p>
+          <label for="<?php echo $this->get_field_name('title'); ?>">
+            <?php _e('Title:'); ?>
+          </label>
+          <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+            name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+        </p>       
+      <?php
     /**
      * /The field where you can insert your Instagram API token
      */
+      ?>
+        <p>
+          <label for="<?php echo $this->get_field_name('instagram_token'); ?>">
+            <?php _e('Instagram Token:'); ?>
+          </label>
+          <input class="widefat" id="<?php echo $this->get_field_id('instagram_token'); ?>"
+            name="<?php echo $this->get_field_name('instagram_token'); ?>" type="text" value="<?php echo esc_attr($instagram_token); ?>" />
+        </p>        
+      <?php
 
 
     }
@@ -107,10 +124,14 @@ class Instagramwidget extends WP_Widget{
         $instance['title'] = (!empty($new_instance['title'])) 
         ? strip_tags($new_instance['title']) 
         : '';
+        /**
+        * /The Method which saves your insertef Instagram API token
+        */
+        $instance['instagram_token'] = (!empty($new_instance['instagram_token'])) 
+        ? strip_tags($new_instance['instagram_token']) 
+        : '';
         
         return $instance;
     }
-    /**
-      * /The Method which saves your insertef Instagram API token
-      */
+
 }
