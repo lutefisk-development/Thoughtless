cfe47a92892f234c0f033f19eeac85da577983f7
Joakim <jmalmberg.web@gmail.com>
2019-12-12 00:43:34 +0100

    added ability to create instagram token with curl

--- public/wp-content/plugins/instagram/includes/class-instagram-widget.php
+++ public/wp-content/plugins/instagram/includes/class-instagram-widget.php
@@ -1,6 +1,7 @@
 <?php
 
 class Instagramwidget extends WP_Widget{
+    public $token = '';
     /**
      * Register Widget with WordPress
      */
@@ -19,20 +20,51 @@ class Instagramwidget extends WP_Widget{
      * @param $api_url
      * @return mixed
      */
-    public function curl_connect( $api_url ){
-      $connection_c = curl_init(); // initializing
-      curl_setopt( $connection_c, CURLOPT_URL, $api_url ); // API URL to connect
-      curl_setopt( $connection_c, CURLOPT_RETURNTRANSFER, 1 ); // return the result, do not print
-      curl_setopt( $connection_c, CURLOPT_TIMEOUT, 20 );
-      $json_return = curl_exec( $connection_c ); // connect and get json data
-      curl_close( $connection_c ); // close connection
-      return json_decode( $json_return ); // decode and return
+    public function curl_post( $uri, $data ){
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $uri); // uri
+      curl_setopt($ch, CURLOPT_POST, true); // POST
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // POST DATA
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // RETURN RESULT true
+      curl_setopt($ch, CURLOPT_HEADER, 0); // RETURN HEADER false
+      curl_setopt($ch, CURLOPT_NOBODY, 0); // NO RETURN BODY false / we need the body to return
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // VERIFY SSL HOST false
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // VERIFY SSL PEER false
+      return json_decode(curl_exec($ch));
+    }
+
+            /**
+     * @param $api_url
+     * @return mixed
+     */
+    public function curl_get( $uri ){
+
+      //Initialize cURL.
+      $ch = curl_init();
+      
+      //Set the URL that you want to GET by using the CURLOPT_URL option.
+      curl_setopt($ch, CURLOPT_URL, $uri);
+      
+      //Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      
+      //Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      
+      return json_decode(curl_exec($ch));
     }
 
     /**
      * Front End display of Widget
      */
     public function widget($args, $instance){
+
+        var_dump(get_site_url());
+
+        var_dump(get_admin_url());
+        $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        var_dump($actual_link);
+
         extract($args);
         $title = apply_filters('widget_title', $instance['title']);
         $instagram_token = apply_filters('widget_title', $instance['instagram_token']);
@@ -52,17 +84,49 @@ class Instagramwidget extends WP_Widget{
 
         <?php
 
-
-        //echo $instance['content'];
+        echo $instance['content'];
         echo $after_widget;
-
-        return $instagram_token;
     }
 
     /**
      * Front End for displaying widget in wp-admin
      */
     public function form($instance) {
+      $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      $url = explode('code=', $actual_link);
+      $code = $url[1];
+
+      if($code) {
+
+        $uri = 'https://api.instagram.com/oauth/access_token'; 
+        $data = [
+          'app_id' => '605693496868185', 
+          'app_secret' => 'd13ff606d23745d00866a0283b2bef92', 
+          'grant_type' => 'authorization_code', 
+          'redirect_uri' => 'https://thoughtless.test/wp-admin/widgets.php',
+          'code' => $code
+        ];
+        $obj_one = $this->curl_post($uri, $data);
+        $token_one = $obj_one->access_token;
+        if($token_one) {
+          $uri = 'https://graph.instagram.com/access_token?grant_type=ig_exchange_token&&client_secret=d13ff606d23745d00866a0283b2bef92&access_token='. $token_one; 
+          $obj_two = $this->curl_get($uri);
+          $token_two = $obj_two->access_token;
+          if($token_two) {
+            ?>
+              <p>
+                <label>
+                  <?php _e('New Token:', 'instagram'); ?>
+                </label>
+                <textarea rows="6" cols="34"><?php echo $token_two; ?></textarea>
+              </p>
+            <?php
+          }
+        }
+      }
+
+        
+
       if (isset($instance['title'])) {
           $title = $instance['title'];
       }
@@ -79,7 +143,7 @@ class Instagramwidget extends WP_Widget{
         $instagram_token = __('New instagram_token', 'instagram');
       }
         /**
-         * The field where you can insert your number of images
+         * The field where you can insert your Instagram token
          */
         if (isset($instance['nr_of_images'])) {
             $nr_of_images = $instance['nr_of_images'];
@@ -111,13 +175,15 @@ class Instagramwidget extends WP_Widget{
             <input class="widefat" id="<?php echo $this->get_field_id('nr_of_images'); ?>"
                    name="<?php echo $this->get_field_name('nr_of_images'); ?>" type="text" value="<?php echo esc_attr($nr_of_images); ?>" />
         </p>
-<!--        --><?php
-//        $url = get_site_url();
-//        ?>
-<!--        <a href="https://api.instagram.com/oauth/authorize?app_id=605693496868185&redirect_uri=--><?php //echo $url; ?><!--/&scope=user_profile,user_media&response_type=code" target="_blank">-->
-<!--          Get code for access token-->
-<!--        <a>-->
-      <?php
+        
+        <?php
+        $url = get_site_url();
+        ?>
+        <a href="https://api.instagram.com/oauth/authorize?app_id=605693496868185&redirect_uri=<?php echo $url; ?>/wp-admin/widgets.php&scope=user_profile,user_media&response_type=code">
+          Get New Instagram Token
+        <a>
+        <?php
+
 
     }
     /**
