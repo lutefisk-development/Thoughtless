001cedfc01baf4a793a5e5db44b4e4b9c935c71b
pks-development <kristiansvanberg@gmail.com>
2019-12-20 10:25:18 +0100

    20-12-2019
    Made changes to global template outlets

--- public/wp-content/themes/understrap/global-templates/outlets.php
+++ public/wp-content/themes/understrap/global-templates/outlets.php
@@ -9,10 +9,9 @@ if(!defined('ABSPATH')) {
 
 $outlets = new WP_Query([
     'post_type'         => 'us_stores',
-    'posts_per_page'    => -1 
+    'posts_per_page'    => -1,
+    'order'             => 'ASC'
 ]);
-
-
 ?>
 
 <section class="wrapper" id="wrapper-outlets">
@@ -22,7 +21,7 @@ $outlets = new WP_Query([
         <!-- conditional to for checking if we have any outlets -->
         <?php if($outlets->have_posts()) : ?>
         
-        <h1><?php _e('This is all our outlets', 'understrap'); ?></h1>
+        <h1 id="page-header"><?php _e('Thoughtless Outlets:', 'understrap'); ?></h1>
         
         <section class="row">
 
@@ -38,6 +37,23 @@ $outlets = new WP_Query([
         <?php endif; ?>
         <!-- Reset postdata back to blog-posts -->
         <?php wp_reset_postdata(); ?>
+
+        <!-- place shortcode for google maps here -->
+        <?php echo do_shortcode("[cm]"); ?>
+
+        <section class="row form-wrapper">
+
+            <article class="col">
+
+                <div class="form">
+
+                    <?php echo do_shortcode("[contact-form-7 id='233' title='test']"); ?>
+
+                </div>
+
+            </article> <!-- /.col -->
+
+        </section> <!-- /.row -->
 
     </main> <!-- /.container -->
 
