af35f1ca8c804ef367f8a82b11f5c82c40c76eb8
Joakim <jmalmberg.web@gmail.com>
2019-12-20 10:57:13 +0100

    Merge branch 'dev' into shop-product-style
    # Conflicts:
    #	public/wp-content/themes/understrap/css/theme.css
    #	public/wp-content/themes/understrap/css/theme.min.css
    #	public/wp-content/themes/understrap/css/theme.min.css.map
    #	public/wp-content/themes/understrap/sass/theme/_thoughtless-store.scss

--- public/wp-content/themes/understrap/loop-templates/content-outlet.php
+++ public/wp-content/themes/understrap/loop-templates/content-outlet.php
@@ -8,19 +8,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<article class="col-md-3 col-sm-12">
+<article class="col-md-4 col-sm-12">
 
     <main class="card">
 
         <div class="card-body">
 
-            <h1 class="card-title"><?php the_title(); ?></h1>
-            <p class="card-text"><?php the_field('store_address'); ?></p>
-            <p class="card-text"><?php the_field('store_postnr'); ?></p>
-            <p class="card-text"><?php the_field('store_city'); ?></p>
+            <h1 class="card-title">
+                <?php the_title(); ?>
+            </h1>
+            <p class="card-text">
+                <?php the_field('store_address'); ?>
+            </p>
+            <p class="card-text">
+                <?php the_field('store_postnr'); ?>
+                &nbsp;
+                <?php the_field('store_city'); ?>
+            </p>
 
         </div> <!-- /.card-body -->
 
     </main> <!-- /.card -->
 
-</article> <!-- /acrticle -->
+</article> <!-- /article -->
