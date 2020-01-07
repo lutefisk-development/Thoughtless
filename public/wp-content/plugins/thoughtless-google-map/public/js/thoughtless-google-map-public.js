28a6f1e2d5dc3d1ab83084fe9637fdb3c914a83d
pks-development <kristiansvanberg@gmail.com>
2020-01-07 13:12:01 +0100

    Added ajax call to wp rest api
    added ajax call to wp rest api, placing map markers based on acf fields.

--- public/wp-content/plugins/thoughtless-google-map/public/js/thoughtless-google-map-public.js
+++ public/wp-content/plugins/thoughtless-google-map/public/js/thoughtless-google-map-public.js
@@ -8,6 +8,7 @@ function initMap() {
 	map 		= $map.get(0),
 	authRequest = new XMLHttpRequest(),
 	get_token   = 'http://localhost/wp/thoughtless/public/wp-json/jwt-auth/v1/token',
+	endpoint    = 'http://localhost/wp/thoughtless/public/wp-json/wp/v2/us_stores',
 	credentials = {
 		'username': thoughtless_google_map_credentials.username,
 		'password': thoughtless_google_map_credentials.password
@@ -37,6 +38,36 @@ function initMap() {
 			
 			// this is the jwt we need to pass, when getting info through wp rest api
 			let token = authJson.token;
+
+			// getting information through wp rest api
+			$.ajax({
+				url: endpoint,
+				success: response => {
+
+					// grabs the acf fields for every store and sets them in a object
+					response.forEach(store => {
+						let cords = {
+							lat: parseFloat(store.acf.store_latitude),
+							lng: parseFloat(store.acf.store_longitude)
+						};
+
+						// for each store place a marker based on the acf fields
+						new google.maps.Marker({
+							position: cords,
+							map: thoughtlessMap,
+							animation: google.maps.Animation.DROP
+						});
+					});
+				},
+				error: error => {
+					console.log(error);
+				},
+
+				// here I add the jwt as authentication
+				beforeSend: xhr => {
+					xhr.setRequestHeader("Authorization", 'Bearer ' + token);
+				},
+			});
 
 		}
 
